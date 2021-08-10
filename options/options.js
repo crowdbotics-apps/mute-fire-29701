2f31359edf6f738ea2ba654b37cadf82f794864c
Crowdbotics <team@crowdbotics.com>
2021-08-10 17:25:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "mute_fire_29701Identifier",
+  url: "https://mute_fire_29701Identifier.botics.co",
+  api: "https://mute_fire_29701Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
